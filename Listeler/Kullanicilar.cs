d290638863641fa253f6bd64667e0a1ee8dc8b78
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-02 17:46:32 +0300

    Listeler ile ilgili alıştırmalar

---
+++ Listeler/Kullanicilar.cs
@@ -0,0 +1,15 @@
+﻿namespace Listeler
+{
+    internal class Kullanicilar
+    {
+
+        string isim;
+        string soyisim;
+        string yas;
+
+        public string Isim { get => isim; set => isim = value; }
+        public string Soyisim { get => soyisim; set => soyisim = value; }
+        public string Yas { get => yas; set => yas = value; }
+
+    }
+}
---
+++ Listeler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listeler
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kullanicilar> kullaniciListesi = new List<Kullanicilar>();
+
+            Kullanicilar kullanici1 = new Kullanicilar();
+            kullanici1.Isim = "Aydin";
+            kullanici1.Soyisim = "AS";
+            kullanici1.Yas = "25";
+
+            Kullanicilar kullanici2 = new Kullanicilar();
+            kullanici2.Isim = "Ördek";
+            kullanici2.Soyisim = "Yaramaz";
+            kullanici2.Yas = "1";
+
+            kullaniciListesi.Add(kullanici1);
+            kullaniciListesi.Add(kullanici2);
+
+
+            foreach (var kullanici in kullaniciListesi)
+            {
+                Console.WriteLine(kullanici.Isim);
+                Console.WriteLine(kullanici.Soyisim);
+                Console.WriteLine(kullanici.Yas);
+
+            }
+        }
+
+        
+    }
+  
+        //liste.Clear(); listeyi temizlemek için kullanılır
+
+        //sayilistesi.Contains(20) sayi listesindeki 20 elemanını bulur
+
+        /*
+        Bir diziyi listeye çevirmek için 
+        string[] hayvanlar = { "Kedi", "Köpek", "Ördek", "Maymun" };
+        List<string> hayvanListesi = new List<string>(hayvanlar);
+        */
+    
+}
