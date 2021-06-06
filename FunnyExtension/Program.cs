cb68335a828d1ee58709827dd82ad7d095f3e345
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-06 18:57:52 +0300

    Interface Segregation Prenciple Example

---
+++ FunnyExtension/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FunnyExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz:");
+            var cumle = Console.ReadLine();
+
+            string komik = cumle.Troll();
+            Console.WriteLine(komik);
+
+
+        }
+    }
+
+    public static class Funny
+    {
+
+        public static string Troll(this string param)
+
+        {
+
+            if (param.Contains("i")) { }
+
+                      
+                return param.Replace("i", "ö");
+
+
+
+        }
+       
+
+    }
+}
---
+++ ISP/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        interface IArac // Yanlış bir kullanım
+        {
+            void motor();
+            void tekerlek();
+            void pervane();
+            void kanat();
+            void kaporta();
+            void yelken();
+        }
+        interface IDenizAracı
+        {
+            void motor();
+            void pervane();
+            void kaporta();
+            void yelken();
+
+        }
+
+        interface IKaraAracı
+        {
+            void tekerlek();
+            void kaporta();
+            void motor();
+
+        }
+        interface IHavaAracı
+        {
+            void tekerlek();
+            void motor();
+            void kanat();
+
+
+        }
+
+        class DenizAraclari : IDenizAracı
+        {
+            public void kaporta()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void motor()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void pervane()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void yelken()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class KaraAraclari : IKaraAracı
+        {
+            public void kaporta()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void motor()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void tekerlek()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        class HavaAraclari : IHavaAracı
+        {
+            public void kanat()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void motor()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void tekerlek()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
