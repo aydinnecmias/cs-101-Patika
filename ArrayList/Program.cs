9a07750336aaa82a2245f86be5787b6e48d32c03
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-07 15:28:22 +0300

    ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList dizi = new ArrayList();
+
+            dizi.Add("Hayvan");
+            dizi.Add("sayi");
+            dizi.Add(2);
+
+            foreach(var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
