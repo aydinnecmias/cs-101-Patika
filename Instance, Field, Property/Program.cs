93badf5e402188e535adf1dd672eb59fdefaa22a
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-07 16:15:16 +0300

    Instance, Field, Property

---
+++ Instance, Field, Property/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Instance__Field__Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Araclar arac1 = new Araclar();
+
+            arac1.Marka = "BMW";
+            arac1.Model = "2021";
+            arac1.Km = 0;
+            arac1.Renk = "Siyah";
+
+            arac1.AracBilgileriGoster();
+
+        }
+    }
+
+    class Araclar
+    {
+        public string Marka;
+        public string Model;
+        public int Km;
+        public string Renk;
+
+        public void AracBilgileriGoster() 
+        {
+            Console.WriteLine("Arac Markası: {0}",Marka );
+            Console.WriteLine("Arac Modeli: {0}", Model);
+            Console.WriteLine("Arac Kmsi: {0}", Km);
+            Console.WriteLine("Arac Renk: {0}", Renk);
+
+        }
+    }
+}
