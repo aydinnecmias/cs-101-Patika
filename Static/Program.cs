ec7af3eb6036019442d89b91c6b597fe49e8a9da
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-07 18:05:12 +0300

    Static Method Example

---
+++ Static/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İlk atanan değer: " + StaticMetot.motor);
+
+            StaticMetot.motor = 60;
+            Console.WriteLine("şimdiki değeri: " + StaticMetot.motor);
+            Console.WriteLine("Otobus deger: " + StaticMetot.Otobus());
+        }
+    }
+
+    class StaticMetot
+    {
+       
+        public static int motor = 1000;
+        public int benzin = 3; 
+
+        
+        public static int Otobus()
+        {
+            
+
+            StaticMetot staticMet = new StaticMetot();
+
+            staticMet.Otomobil(); 
+            staticMet.benzin = 2;
+            return motor / 2;
+        }
+
+        public void Otomobil() 
+        {
+
+        }
+    }
+
+    
+
+    
+    }
+
