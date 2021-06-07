3cb39d85f8ef19d0318fe5bd130308f5a20ab995
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-07 19:18:27 +0300

    Enum Example

---
+++ EnumExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EnumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazartesi);
+            
+            int bugun =7;           
+
+            if (bugun == (int)Gunler.Pazar)
+            {
+                Console.WriteLine("Tatil günü");
+            }
+            else
+            {
+                Console.WriteLine("tatil yok");
+            }
+
+
+        }
+
+        
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+}
