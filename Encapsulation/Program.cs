d7e74fe41f5c9d7d15a61ab61d83908c1b2c863c
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-06-07 17:31:01 +0300

    Encapsulation

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Personel
+
+        
+
+    {
+        private DateTime _GoreveBaslamaTarih;
+        public DateTime GoreveBaslamaTarih
+        {
+            
+            get { return _GoreveBaslamaTarih; }
+            
+            set
+            {
+                if (_GoreveBaslamaTarih.Year < DateTime.Now.Year)
+                    Console.WriteLine("Hatalı Göreve Başlama Tarih Bilgisi");
+                    _GoreveBaslamaTarih = value;
+            }
+        }
+    }
+}
